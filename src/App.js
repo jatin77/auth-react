4d22be598f367fc3fbf858bfbab3b68d98257bcc
unknown <jatin@quoality.com>
2021-06-16 15:53:49 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './pages/Auth/Login';
+import { Redirect, Switch } from 'react-router-dom';
+import { ProtectedRoute, UnprotectedRoute } from './route/routeCheck';
+import { HOME_PATH, LOGIN_PATH } from './route/routePaths';
+import Home from './pages/Home';
+import { connect } from 'react-redux';
 
-function App() {
+function App({ user }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <UnprotectedRoute
+        path={LOGIN_PATH}
+        component={Login}
+        loggedInPath={HOME_PATH}
+        exact
+      />
+      <ProtectedRoute path={HOME_PATH} exact component={Home} />
+      <Redirect to='/' />
+    </Switch>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return { user: state.user };
+};
+
+export default connect(mapStateToProps)(App);
